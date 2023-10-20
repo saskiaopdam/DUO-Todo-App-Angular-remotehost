2d622b363f340ea3e93afc08ad5d2743cea62771
saskiaopdam <opdamsaskia@icloud.com>
2023-10-20 21:30:42 +0200

    UX - make list sort on 'checked' + hide del-buttons

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { tap, map } from "rxjs/operators";
 
 import { Todo } from "../todo.model";
-import { Observable } from "rxjs";
+
 import {
   requestAddAction,
+  requestUpdateAction,
   requestDeleteAction,
   requestLoadAction,
-  requestToggleAction, requestUpdateAction
+  requestToggleAction,
 } from "../todo.actions";
 import { AppState } from "../todo.reducer";
 
@@ -17,15 +20,56 @@ import { AppState } from "../todo.reducer";
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  @ViewChild('editInput') editInput!: ElementRef;
+  @ViewChild('addingInput') addingInput!: ElementRef;
 
   todo$: Observable<Todo[]>;
+  sortedTodos$: Observable<Todo[]>;
   editing: boolean = false;
   editingTodo: any;
   adding: boolean = false;
   addingTodo: any;
+  deleting: boolean = false;
 
   constructor(private store: Store<AppState>) {
     this.todo$ = store.pipe(select('todos'));
+
+    // sort by 'checked' property (unchecked first)
+    this.sortedTodos$ = this.todo$.pipe(
+      tap(todos => console.log('Original Todos:', todos)),
+      map(todos => {
+        return todos.slice().sort((a, b) => {
+          if (!a.checked && b.checked) {
+            return -1; // 'a' comes before 'b'
+          } else if (a.checked && !b.checked) {
+            return 1; // 'b' comes before 'a'
+          } else {
+            return 0; // order remains unchanged
+          }
+        });
+      }),
+      tap(sortedTodos => console.log('Sorted Todos:', sortedTodos))
+    );
+
+    // first sort by 'checked', then sort by 'task'
+    // this.sortedTodos$ = this.todo$.pipe(
+    //   tap(todos => console.log('Original Todos:', todos)),
+    //   map(todos => {
+    //     return todos.slice().sort((a, b) => {
+    //       // Sort by checked property first (unchecked items first)
+    //       if (!a.checked && b.checked) {
+    //         return -1; // 'a' comes before 'b'
+    //       } else if (a.checked && !b.checked) {
+    //         return 1; // 'b' comes before 'a'
+    //       } else {
+    //         // If checked status is the same, sort alphabetically by 'task'
+    //         return a.task.localeCompare(b.task);
+    //       }
+    //     });
+    //   }),
+    //   tap(sortedTodos => console.log('Sorted Todos:', sortedTodos))
+    // );
+
   }
 
   ngOnInit(): void {
@@ -47,6 +91,11 @@ export class TodoListComponent implements OnInit {
     // console.log('editing todo: ' + this.editingTodo.task);
     this.editing = true;
     // console.log('editing: ' + this.editing);
+
+    // After setting editingTodo and other logic, focus the input field
+    setTimeout(() => {
+      this.editInput.nativeElement.focus();
+    });
   }
 
   onEditDone() {
@@ -62,6 +111,11 @@ export class TodoListComponent implements OnInit {
     // console.log('adding todo: ' + this.addingTodo.task);
     this.adding = true;
     // console.log('adding: ' + this.adding);
+
+    // After setting addingTodo and other logic, focus the input field
+    setTimeout(() => {
+      this.addingInput.nativeElement.focus();
+    });
   }
 
   onAddingDone() {
@@ -72,7 +126,15 @@ export class TodoListComponent implements OnInit {
     console.log('adding: ' + this.adding);
   };
 
+  startDeleting() {
+    this.deleting = true;
+  };
+
   delete(id: number): void {
     this.store.dispatch(requestDeleteAction({ id: id }));
+  }
+
+  onDeleteDone(): void {
+    this.deleting = false;
   }
 }
