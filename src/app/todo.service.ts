51e72ef0b081717f17086e2950e013eeb69e1960
Saskia Opdam <opdamsaskia@icloud.com>
2024-07-27 12:31:25 +0200

    Update todo.service.ts - set stackhero mariadb urls

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -16,24 +16,25 @@ export class TodoService {
   constructor(public http: HttpClient) { }
 
   add(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>('http://localhost:8080/todo', todo);
+    // return this.http.post<Todo>('http://localhost:8080/todo', todo);
+    return this.http.post<Todo>('http://m1y7g7.stackhero-network.com:4656/todo', todo);
   }
 
   update(todo: Todo): Observable<Todo> {
-    return this.http.put<Todo>('http://localhost:8080/todo/' + todo.id, todo
+    return this.http.put<Todo>('http://m1y7g7.stackhero-network.com:4656/todo/' + todo.id, todo
     );
   }
 
   delete(id: number) {
-    return this.http.delete('http://localhost:8080/todo/' + id)
+    return this.http.delete('http://m1y7g7.stackhero-network.com:4656/todo/' + id)
   }
 
   load(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('http://localhost:8080/todo')
+    return this.http.get<Todo[]>('http://m1y7g7.stackhero-network.com:4656/todo')
   }
 
   toggle(todo: Todo): Observable<Todo> {
-    return this.http.put<Todo>('http://localhost:8080/todo/' + todo.id, todo
+    return this.http.put<Todo>('http://m1y7g7.stackhero-network.com:4656/todo/' + todo.id, todo
     );
   }
 
